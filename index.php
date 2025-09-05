9cc02bbe4359aba34ba47c1616b999014ccc4539
Kirill Potapov <kirill.potapov@tptlive.ee>
2025-09-05 09:46:29 +0300

    added_basics

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Hello World!\n";
+echo("Hello world");
+echo "\n";
+$name = "Kirill";
+echo 'Hello $name';
+echo "\nHello $name\n";
+
+$var = 'HellÕ';
+var_dump($var);
+var_dump(strlen($var));
+$var = 1;
+var_dump($var);
+$var = 1.5;
+var_dump($var);
+var_dump(PHP_INT_MAX);
+var_dump(PHP_FLOAT_MAX);
+
+$var = true; 
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = NULL;
+var_dump($bar);
+?>
