6422d7173979baddc1fbd7458750a7e441242277
Kirill Potapov <kirill.potapov@tptlive.ee>
2025-09-05 10:44:08 +0300

    booladded

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 
-$num = 1;
-
-$num = $num + 1;
-$num = $num - 1;
-$num = $num * 2;
-$num = $num / 2;
-$num = 14;
-$num = $num % 4;
-$num = $num **2;
-var_dump($num);
-$num += 1;
-$num -= 1;
-$num /= 2;
-$num *= 2;
-$num %= 2;
-$num **= 2;
+$bool = true && false; 
+var_dump($bool);
+$bool = true || false;
+var_dump($bool);
+$bool = !false;
+var_dump($bool);
+$bool = (true || false && true) && (!false || false && true);
+var_dump($bool);
+$bool = 10 > 4;
+$bool = 10 < 4;
+$bool = 10 > 10;
+$bool = 10 < 10; 
+$bool = 10 == 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 != '10';
+$bool = 10 !== '10';
+$bool = 10 <= '10';
+$bool = 10 >= '10';
+var_dump($bool);
