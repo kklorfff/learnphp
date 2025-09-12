ee8d84dcc76ef0c996e99b6ef9c889a172ad89e8
Kirill Potapov <kirill.potapov@tptlive.ee>
2025-09-12 11:27:51 +0300

    add visibility example

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 class Box {
    public $width;
-   public $height;
-   public $lenght;
+   protected $height;
+   private $length;
 
    public function volume(){
-        return $this->width * $this-> height * $this->lenght;
+        return $this->width * $this-> height * $this->length;
     }
 }
 
@@ -14,16 +14,20 @@ class MetalBox extends Box {
     public $material = 'Metal';
     public $weightPerUnit = 2;
 
+    public function test (){
+        var_dump($this->length);
+    }
+
     public function mass() {
         return $this->volume() * $this->weightPerUnit;
     }
 }
 
 $metalBox1 = new MetalBox();
-$metalBox1->width = 10;
+$metalBox1->width = 'a lot';
+// $metalBox1->test();
 $metalBox1->height = 20;
-$metalBox1->lenght = 30;
+$metalBox1->length = 30;
 
 var_dump($metalBox1->volume());
-var_dump($metalBox1->mass());
 var_dump($metalBox1);
