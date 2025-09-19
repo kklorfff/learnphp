5e385a1b5b520c84821b2687708f5e5d88db7b8b
Kirill Potapov <kirill.potapov@tptlive.ee>
2025-09-19 11:38:38 +0300

    Split blog example into partials

--- us.php
+++ us.php
@@ -1,4 +1,7 @@
 <?php include __DIR__. '/partials/header.php'; ?>
+<?php include __DIR__. '/partials/themeselect.php'; ?>
+<?php include __DIR__. '/partials/navbar.php'; ?>
+  
 
 
     <main class="container">
@@ -8,10 +11,4 @@
       <div class="row g-5">
         <div class="col-md-8">
       <?php include __DIR__. '/partials/posts.php'; ?>
-        </div>
-        <div class="col-md-4">
-               <?php include __DIR__. '/partials/sidebar.php'; ?>
-        </div>
-      </div>
-    </main>
-    <?php include __DIR__. '/partials/footer.php'; ?>
+
